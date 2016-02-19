748a5a39d051b324ca25c7c7dbd5a92183247652
Miguel Esteban Gutiérrez <m.esteban.gutierrez@gmail.com>
2016-02-19 17:21:29 +0100

    Started provision of utils. for carrying out integration tests, continued
    - Added Null implementations of the object model.

---
+++ backend/src/test/java/org/smartdeveloperhub/harvesters/scm/testing/enhancer/NullCommitterState.java
@@ -0,0 +1,64 @@
+/**
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ *   This file is part of the Smart Developer Hub Project:
+ *     http://www.smartdeveloperhub.org/
+ *
+ *   Center for Open Middleware
+ *     http://www.centeropenmiddleware.com/
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ *   Copyright (C) 2015-2016 Center for Open Middleware.
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *             http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ *   Artifact    : org.smartdeveloperhub.harvesters.scm:scm-harvester-frontend:0.3.0-SNAPSHOT
+ *   Bundle      : scm-harvester.war
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ */
+package org.smartdeveloperhub.harvesters.scm.testing.enhancer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.smartdeveloperhub.harvesters.scm.backend.pojos.User;
+
+final class NullCommitterState implements CommitterState {
+
+	private static final Logger LOGGER=LoggerFactory.getLogger(State.class);
+
+	private final String id;
+
+	NullCommitterState(final String committerId) {
+		this.id = committerId;
+	}
+
+	@Override
+	public String getId() {
+		return this.id;
+	}
+
+	@Override
+	public String getName() {
+		return "<unknown>";
+	}
+
+	@Override
+	public User toEntity() {
+		LOGGER.debug("Unknown committer {}: cannot return representation",this.id);
+		return null;
+	}
+
+	@Override
+	public void logActivity(final long timestamp) {
+		LOGGER.debug("Unknown committer {}: cannot log activity ({})",this.id,timestamp);
+	}
+
+}
