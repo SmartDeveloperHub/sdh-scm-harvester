720861dd7e951abe40ab20fb89d001c402f4ee07
Miguel Esteban Gutiérrez <m.esteban.gutierrez@gmail.com>
2016-02-22 15:31:44 +0100

    Started provision of utils. for carrying out integration tests, continued
    - Added more Undertow HttpHandlers for exposing an endpoint for submitting
      the Git Collector Notification events, and delivering it to the GitLab
      Enhancer mock.

---
+++ backend/src/test/java/org/smartdeveloperhub/harvesters/scm/testing/handlers/ContentTypeConsumerHandler.java
@@ -0,0 +1,81 @@
+/**
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ *   This file is part of the Smart Developer Hub Project:
+ *     http://www.smartdeveloperhub.org/
+ *
+ *   Center for Open Middleware
+ *     http://www.centeropenmiddleware.com/
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ *   Copyright (C) 2015-2016 Center for Open Middleware.
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *             http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ *   Artifact    : org.smartdeveloperhub.harvesters.scm:scm-harvester-backend:0.3.0-SNAPSHOT
+ *   Bundle      : scm-harvester-backend-0.3.0-SNAPSHOT.jar
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ */
+package org.smartdeveloperhub.harvesters.scm.testing.handlers;
+
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.HeaderValues;
+import io.undertow.util.Headers;
+import io.undertow.util.StatusCodes;
+
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+
+final class ContentTypeConsumerHandler extends HandlerUtil implements HttpHandler {
+
+	private final Set<String> mimes;
+	private HttpHandler next;
+
+	private ContentTypeConsumerHandler() {
+		this.mimes=Sets.newLinkedHashSet();
+	}
+
+	ContentTypeConsumerHandler consumes(final String mime) {
+		if(mime!=null) {
+			this.mimes.add(mime);
+		}
+		return this;
+	}
+
+	ContentTypeConsumerHandler setNext(final HttpHandler aHandler) {
+		this.next=aHandler;
+		return this;
+	}
+
+	@Override
+	public void handleRequest(final HttpServerExchange exchange) throws Exception {
+		final HeaderValues contentTypeHeader = exchange.getRequestHeaders().get(Headers.CONTENT_TYPE);
+		if(contentTypeHeader==null || contentTypeHeader.isEmpty()) {
+			fail(exchange,StatusCodes.UNSUPPORTED_MEDIA_TYPE,"No Content-Type specified");
+		} else {
+			final Set<String> supportedMimes=Sets.newHashSet(contentTypeHeader);
+			supportedMimes.retainAll(this.mimes);
+			if(supportedMimes.isEmpty()) {
+				fail(exchange,StatusCodes.UNSUPPORTED_MEDIA_TYPE,"Expected Content-Type one of %s not one of %s",this.mimes,contentTypeHeader);
+			} else {
+				if(this.next!=null) {
+					this.next.handleRequest(exchange);
+				}
+			}
+		}
+	}
+
+	static ContentTypeConsumerHandler create() {
+		return new ContentTypeConsumerHandler();
+	}
+}
---
+++ backend/src/test/java/org/smartdeveloperhub/harvesters/scm/testing/handlers/GitCollectorEventProcessorHandler.java
@@ -0,0 +1,90 @@
+/**
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ *   This file is part of the Smart Developer Hub Project:
+ *     http://www.smartdeveloperhub.org/
+ *
+ *   Center for Open Middleware
+ *     http://www.centeropenmiddleware.com/
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ *   Copyright (C) 2015-2016 Center for Open Middleware.
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *             http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ *   Artifact    : org.smartdeveloperhub.harvesters.scm:scm-harvester-backend:0.3.0-SNAPSHOT
+ *   Bundle      : scm-harvester-backend-0.3.0-SNAPSHOT.jar
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ */
+package org.smartdeveloperhub.harvesters.scm.testing.handlers;
+
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.StatusCodes;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.smartdeveloperhub.harvesters.scm.backend.notification.Event;
+import org.smartdeveloperhub.harvesters.scm.testing.enhancer.GitLabEnhancer;
+import org.smartdeveloperhub.harvesters.scm.testing.enhancer.GitLabEnhancer.UpdateReport;
+
+final class GitCollectorEventProcessorHandler extends HandlerUtil implements HttpHandler {
+
+	private static final Logger LOGGER=LoggerFactory.getLogger(GitCollectorEventProcessorHandler.class);
+
+	private Event event;
+
+	private final GitLabEnhancer enhancer;
+
+	GitCollectorEventProcessorHandler(final GitLabEnhancer enhancer) {
+		this.enhancer = enhancer;
+	}
+
+	Event getEvent() {
+		return this.event;
+	}
+
+	GitCollectorEventProcessorHandler setEvent(final Event event) {
+		this.event = event;
+		return this;
+	}
+
+	@Override
+	public void handleRequest(final HttpServerExchange exchange) {
+		try {
+			Event processEvent=this.event;
+			String action="ignored";
+			final UpdateReport report = this.enhancer.update(processEvent);
+			if(report.wasSuccesful()) {
+				processEvent=report.curatedEvent();
+				action="sent";
+			}
+			final String entity=marshall(processEvent);
+			answer(exchange,StatusCodes.OK, "Notification %s:\n%s",action,entity);
+			LOGGER.debug("Notification {} {}:\n{}",processEvent.getClass().getSimpleName(),action,entity);
+		} catch (final Throwable e) {
+			fail(exchange,e,"Could not update enhancer");
+		}
+	}
+
+	private String marshall(final Event event) {
+		try {
+			return JsonUtil.marshall(event);
+		} catch (final IOException e) {
+			LOGGER.debug("Could not serialize response event",e);
+			return event.toString();
+		}
+	}
+
+
+}
